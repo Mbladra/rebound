5e37a7f92dc6510f25383b75495276e5fc7fcb92
Malik <mdrabla1@gmail.com>
2018-04-23 11:58:18 -0400

    Changes made to code clarity

--- setup.py
+++ setup.py
@@ -11,6 +11,16 @@ with open("README.md", encoding="utf-8") as file:
     readme = file.read()
 
 setup(
+    
+    keywords="stackoverflow stack overflow debug debugging error-handling compile errors error message cli",
+    include_package_data=True,
+    packages=["rebound"],
+    #data_files=["demo.gif"],
+    entry_points={"console_scripts": ["rebound = rebound.rebound:main"]},
+    install_requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
+    requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
+    python_requires=">=3", # NOTE: This will change
+    license="MIT"
     name="rebound-cli",
     version="1.1.7a1",
     description="Automatically displays Stack Overflow results when you get a compiler error",
@@ -29,13 +39,4 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python"
     ],
-    keywords="stackoverflow stack overflow debug debugging error-handling compile errors error message cli",
-    include_package_data=True,
-    packages=["rebound"],
-    #data_files=["demo.gif"],
-    entry_points={"console_scripts": ["rebound = rebound.rebound:main"]},
-    install_requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
-    requires=["BeautifulSoup4", "requests", "urllib3", "urwid"],
-    python_requires=">=3", # NOTE: This will change
-    license="MIT"
 )
